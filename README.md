ccdf0583d8d3911138d9d4da1a3e67535a58c979
nanstis <bleyo@alphomega.org>
2026-09-23 22:56:43 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Project
+
+## uikit 
+composer config repositories.ui-kit vcs https://github.com/alpha-omega-corp/laravel
+composer require --dev alpha-omega-corp/ui-kit:dev-production
+
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ __('Welcome') }} - {{ config('app.name', 'Laravel') }}</title>
+
+        <link rel="icon" href="/favicon.ico" sizes="any">
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
+        @fonts
+
+    </head>
+    <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+        <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
+            <main class="flex max-w-83.75 w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
+
+            </main>
+        </div>
+
+        @if (Route::has('login'))
+            <div class="h-14.5 hidden lg:block"></div>
+        @endif
+    </body>
+</html>
